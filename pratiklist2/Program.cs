17e6fd2713addfbdd495aa996c3ef53272254598
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-08-14 01:32:54 +0300

    Proje dosyası ekle.

---
+++ pratiklist2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Saldırbaksa hoş geldiniz ");
+List<string> list = new List<string>();
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"{i}. Kahve --> ");
+    string kahveisim = Console.ReadLine();
+    list.Add( kahveisim );
+}
+
+Console.WriteLine("!!!! SALDIRBAKS KAHVE ÇEŞİTLERİ !!!!");
+foreach (var kahve in list)
+{
+    Console.WriteLine(kahve);
+}
